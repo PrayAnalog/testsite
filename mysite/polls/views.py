c9e8c7b61d9ca37b8262779c25605e860d928597
PrayAnalog <agy0304@gmail.com>
2016-02-19 07:34:20 +0000

    error?

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import get_object_or_404, render
-from .models import Choice, Question
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.view import generic
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list' : latest_question_list, }
-    return render(request, 'polls/index.html', context)
+from .models import Choice, Question
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question':question})
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
+
+	def get_queryset(self):
+		return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.ListView):
+	model = Question
+	template_name = 'polls/detail.html'
+
+class ResultView(generic.ListView):
+	model = Question
+	template_name = 'polls/results.html'
 
 def vote(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
@@ -25,6 +30,5 @@ def vote(request, question_id):
 	else:
 		selected_choice.votes += 1
 		selected_choice.save()
-
 		return HttpResponseRedirect(reverse('polls:results', args=(question.id,))) 
 # Create your views here.
